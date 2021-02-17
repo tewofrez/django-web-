a4693dfe99a2695ac3fe63c61a43f5e272253e66
TEWODROS ASMAMAW <52701533+tewofrez@users.noreply.github.com>
2021-02-17 04:38:59 -0800

    add some css file and python function
    is this new year? python function

--- myportfolio/hello_worled/views.py
+++ myportfolio/hello_worled/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "html_file/index.html")
+    now = datetime.datetime.now()
+    return render(request, "html_file/index.html", {
+        "newyear": now.month == 1 and now.day == 11
+    })
